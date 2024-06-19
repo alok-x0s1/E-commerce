4388987c4145dbc8f050711a4ba7af8ede22b3d7
Alok Yadav <yalok6321@gmail.com>
2024-06-19 11:38:48 +0530

    spelling mistakes

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,14 +5,14 @@ const Product = require("../models/product.model");
 
 router.post("/create", upload.single("image"), async (req, res) => {
   const { name, bgColor, pannelColor, textColor, price, discount } = req.body;
-  // if (
-  //   [name, bgColor, pannelColor, textColor, price].some(
-  //     (field) => !field?.trim()
-  //   )
-  // ) {
-  //   req.flash("error", "Something went wrong : All fields are required.");
-  //   return res.status(400).send("All fields are required");
-  // }
+  if (
+    [name, bgColor, pannelColor, textColor, price].some(
+      (field) => !field?.trim()
+    )
+  ) {
+    req.flash("error", "Something went wrong : All fields are required.");
+    return res.status(400).send("All fields are required");
+  }
 
   try {
     let product = await Product.create({
